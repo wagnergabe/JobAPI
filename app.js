62b06aa47777eb2fe7dcc33d1caf23f5aa0d3547
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-09-04 22:20:50 -0500

    connnected to database

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connect DB
+const connectDB = require('./db/connect')
+
 //routers
 
 const authRouter = require('./routes/auth')
@@ -23,6 +26,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
