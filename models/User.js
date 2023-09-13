27dc565d6a93dd757d12a2d023a5b97cb37e43b4
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-09-12 21:26:50 -0500

    create auth middleware

--- models/User.js
+++ models/User.js
@@ -1,40 +1,43 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please provide a name"],
-    minLength: 3,
+    required: [true, 'Please provide name'],
     maxlength: 50,
+    minlength: 3,
   },
   email: {
     type: String,
-    required: [true, "Please provide an email address"],
+    required: [true, 'Please provide email'],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email",
+      'Please provide a valid email',
     ],
     unique: true,
   },
   password: {
     type: String,
-    required: [true, "Please provide a password"],
-    minLength: 6,
+    required: [true, 'Please provide password'],
+    minlength: 6,
   },
-});
+})
 
-UserSchema.pre("save", async function () {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 UserSchema.methods.createJWT = function () {
-    return jwt.sign({userId:this._id,name:this.name}, 'jwtSecret', {
-        expiresIn: '30d',
-    })
-
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  )
 }
 
 UserSchema.methods.comparePassword = async function (canditatePassword) {
@@ -42,4 +45,4 @@ UserSchema.methods.comparePassword = async function (canditatePassword) {
   return isMatch
 }
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema)
