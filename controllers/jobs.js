8d6f26a8df7289fdca6defa8549629ed0cccf474
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-09-17 20:57:23 -0500

    update controller complete

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -24,9 +24,26 @@ const createJob = async(req, res) => {
     res.status(StatusCodes.CREATED).json({job})
 }
 
-const updateJob = async(req, res) => {
-    res.send('update job')
-}
+cconst updateJob = async (req, res) => {
+    const {
+      body: { company, position },
+      user: { userId },
+      params: { id: jobId },
+    } = req
+  
+    if (company === '' || position === '') {
+      throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+    const job = await Job.findByIdAndUpdate(
+      { _id: jobId, createdBy: userId },
+      req.body,
+      { new: true, runValidators: true }
+    )
+    if (!job) {
+      throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
+  }
 
 const deleteJob = async ( req, res ) => {
     res.send('delete job')
